ae982b05a0bc6331f0eeab2707bbc8fd7759dc4b
MERCADIER <celine.mercadier@capgemini.com>
2026-02-02 11:13:12 +0100

    ajout life, increment et decrement life, img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,24 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Hola';
+  name = signal("Pikachu");
+  life = signal(21);
+  imageSrc = signal("../assets/img/pikachu.png");
+
+  size = computed(() => {
+    //Taille est un dérivé de life --> computed()
+    const life = this.life();
+
+    if (life < 15) return 'Petit';
+    if (life < 25) return 'Moyen';
+    return 'Grand';
+  });
+
+
+  incrementLife(){
+    this.life.update(v => v+1);
+  }
+  decrementLife(){
+    this.life.update(v => v-1);
+  }
 }
