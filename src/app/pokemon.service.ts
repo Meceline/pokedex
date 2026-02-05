85df2c4e8075debaca17efa491d58161ad61a02e
Meceline <celinemercadier@gmail.com>
2026-02-05 11:28:17 +0100

    ServicePokemon service : recupérer un pokémon par son id, la liste des pokemon, la liste des types

---
+++ src/app/pokemon.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Pokemon, PokemonList } from './pokemon.interface';
+import { POKEMON_LIST } from './pokemon-list.fake';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  getPokemonList(): PokemonList{
+    return POKEMON_LIST;
+  }
+  getPokemonById(id: number): Pokemon{
+    const pokemon = POKEMON_LIST.find(pokemon => pokemon.id === id);
+    if(!pokemon){
+      throw new Error("No pokemon with id : ${id}");
+    }
+    return pokemon;
+  }
+  getPokemonTypeLis(): string[]{
+    return [
+      'Plante',
+      'Feu',
+      'Eau', 
+      'Insecte',
+      'Normal',
+      'Electrik',
+      'Poison',
+      'Fée',
+      'Vol'
+    ];
+  }
+}
