5ef07148b54de4915aa72fd860e9239cb8ad70dc
Meceline <celinemercadier@gmail.com>
2026-02-09 16:03:30 +0100

    ajout detail dans la page profile, page 404

--- src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
+++ src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { PokemonService } from '../../pokemon.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-profile',
-  imports: [],
+  imports: [RouterLink, DatePipe],
   templateUrl: './pokemon-profile.component.html',
   styles: ``
 })
 export class PokemonProfileComponent {
+  readonly #router = inject(ActivatedRoute);
+  readonly #pokemonService = inject(PokemonService);
+  readonly pokemon = signal(this.#pokemonService.getPokemonById(Number(this.#router.snapshot.params['id'])));
+  
+  //readonly #pokemonId = Number(this.#router.snapshot.paramMap.get('id'));
+  //readonly pokemon = signal(this.#pokemonService.getPokemonById(this.#pokemonId));
+
 
 }
